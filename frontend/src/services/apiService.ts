1b820a2f0a33a626f38ef4de08a6438c414e2422
Asamatter <jph8041@gmail.com>
2024-01-07 11:16:12 -0500

    update files

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get("https://backend-production-7211.up.railway.app/api/tasks/");
+    const response = await axios.get("https://backend-production-7211.up.railway.app/api/tasks");
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
